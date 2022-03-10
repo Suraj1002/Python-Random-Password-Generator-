5a87abb163e7eceb380730821100ecbf2f029d4f
Suraj1002 <101330027+Suraj1002@users.noreply.github.com>
2022-03-10 17:24:28 +0530

    Add files via upload

---
+++ random password generator.py
@@ -0,0 +1,18 @@
+import random
+
+length=int(input("Enter length of password: "))
+
+all_lists = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z','@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+           '*', '(', ')', '<']
+
+password=""
+
+for i in range(length):
+    password=password +random.choice(all_lists)
+print(password)
